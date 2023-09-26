593bc25bce7dc057c22cfd6b7656dd78041259e4
Supawitsaelim <oatsupawit_159@outlook.com>
2023-09-26 08:46:06 +0700

    up

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const path = require('path');
 var bodyParser = require('body-parser');
 
 const base_url = 'http://127.0.0.1:3000';
 
+app.set('views', path.join(__dirname, '/public/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
